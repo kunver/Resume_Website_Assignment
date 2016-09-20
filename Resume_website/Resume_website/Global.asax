1b15f8a6e502ed0497b4b5c1c5c34535352033da
300693183 <300693183@PR-ICET-B317-28>
2016-09-20 09:02:18 -0400

    Added the groundwork

---
+++ Resume_website/Resume_website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Resume_website.Global" Language="C#" %>
---
+++ Resume_website/Resume_website/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resume_website.Startup))]
+namespace Resume_website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
